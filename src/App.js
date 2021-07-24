f334748594e916de9e7c9ab39c840671855e7727
Mizanur Rahman <mizzanur007@gmail.com>
2021-07-24 14:39:18 +0600

    Finished Header nav part

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from "react";
-import {
-  BrowserRouter as Router, Route, Switch
-} from "react-router-dom";
-import MainAbout from "./components/AboutPage/MainAbout/MainAbout";
-import Home from "./components/HomePage/MainHome/MainHome";
-import Footer from "./components/Shared/Footer";
-import Navbar from "./components/Shared/Navbar";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MainAbout from './components/AboutPage/MainAbout/MainAbout';
+import Home from './components/HomePage/MainHome/MainHome';
+import Footer from './components/Shared/Footer';
+import Header from './components/Shared/Header/Header';
 
 export default function App() {
   return (
     <Router>
-      <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <MainAbout />
-          </Route>
-        </Switch>
-        <Footer />
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <MainAbout />
+        </Route>
+      </Switch>
+      <Footer />
     </Router>
   );
 }
